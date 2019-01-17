0e9c4ad0a4d2dd06225e44db30a756778aaa5754
nirmaAtma <46768376+nirmaAtma@users.noreply.github.com>
2019-01-16 21:08:44 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+"""test program"""
+def main():
+    print('Guess my number between 1 and 100')
+    randomNumber = 44
+    random = 55
+    userGuess = input("Your Guess: ")
+    if (float(userGuess) == randomNumber+random):
+        print ("YESS")
+    else:
+        print ("NOOO")
+    
+        
+if __name__ == "__main__":
+    main()
